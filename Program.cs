e42c63455457e7a7fa0ce7d518367e42b3048a23
Tw212 <Tewe07001@utb.vaxjo.se>
2024-09-06 12:57:11 +0200

    loopar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*int tal = 1;
+while(tal<=20)
+{
+    Console.WriteLine(tal);
+    tal++;
+
+}*/
+
+
+Console.WriteLine("säg ett tal mellan 1-101");
+int tal= int.Parse(Console.ReadLine());
+while(tal<=101)
+{
+Console.WriteLine(tal);
+  tal++;
+}
